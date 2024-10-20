860569552c9ada59f94cd5328f9af587ad654b44
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-20 10:31:53 +0100

    first commit

---
+++ src/test/java/com/app/web_reactive/WebReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.web_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
